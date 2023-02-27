15beb22517a2ac2decd49d45fb69db3d262fe957
fivesun1228@163.com <fivesun1228@163.com>
2023-02-27 23:00:58 +0800

    update

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,28 +1,34 @@
+import { JwtAuthGuard } from './../auth/jwt-auth.guard';
 import { Tags } from 'src/tags/tags.entity';
 import { TagsService } from './tags.service';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 
 @Controller('tags')
 export class TagsController {
   constructor(private tagsService: TagsService) {}
+
+  @UseGuards(JwtAuthGuard)
   @Post('')
   query(@Body() body) {
     const { userId, page, pageSize } = body;
     return this.tagsService.findAll(userId, page, pageSize);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post('create')
   createTag(@Body() body: Tags) {
     const { name, sign, userId } = body;
     return this.tagsService.createTag(name, sign, userId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post('update')
   updateTag(@Body() body: Tags) {
     const { name, sign, userId } = body;
     return this.tagsService.updateTag(name, sign, userId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post('delete')
   deleteTag(@Body() body) {
     const { id, userId } = body;
